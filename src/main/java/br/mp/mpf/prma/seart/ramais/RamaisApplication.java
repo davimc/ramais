379f0a13913852c16667a01487cc53da90bd34d3
davimc <davimatosc@hotmail.com>
2020-01-23 12:51:45 -0300

    first commit

---
+++ src/main/java/br/mp/mpf/prma/seart/ramais/RamaisApplication.java
@@ -0,0 +1,13 @@
+package br.mp.mpf.prma.seart.ramais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/mp/mpf/prma/seart/ramais/RamaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mp.mpf.prma.seart.ramais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

db6dff70a11e4d65677f6e12f2e4a96cc9a33873
davimc <davimatosc@hotmail.com>
2020-01-29 09:54:44 -0300

    ajeitando repository

--- src/main/java/br/mp/mpf/prma/seart/ramais/repositories/TelefoneRepository.java
+++ src/main/java/br/mp/mpf/prma/seart/ramais/repositories/TelefoneRepository.java
@@ -1,5 +1,6 @@
 package br.mp.mpf.prma.seart.ramais.repositories;
 
+import br.mp.mpf.prma.seart.ramais.domain.Contato;
 import br.mp.mpf.prma.seart.ramais.domain.Setor;
 import br.mp.mpf.prma.seart.ramais.domain.Telefone;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,9 +8,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
-
+    @Query("SELECT c FROM contato c WHERE telefone_id = ?")
+    public Optional<Contato> retornaContatosPorTelefone(long telefoneId);
     
 }

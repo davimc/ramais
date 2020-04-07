d0de76e24824c6205706a8420f2b67515ae42867
davimc <davimatosc@hotmail.com>
2020-04-07 18:41:54 -0300

    adicionando index

--- src/main/java/br/mp/mpf/prma/seart/ramais/controllers/index.java
+++ src/main/java/br/mp/mpf/prma/seart/ramais/controllers/index.java
@@ -1,12 +1,17 @@
 package br.mp.mpf.prma.seart.ramais.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class index {
+    @Autowired
+    ContatoController contatoController;
+    @GetMapping
     public void index(){
-
+        contatoController.listaContatoPeloNome("");
     }
 }
